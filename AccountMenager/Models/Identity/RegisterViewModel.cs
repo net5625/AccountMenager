f6ecaddbcc70a92974377d25fb7bdc6554ec3dae
Sylwester <net5625@wp.pl>
2021-12-05 16:39:25 +0100

    Regex didn't working good.

--- AccountMenager/Models/Identity/RegisterViewModel.cs
+++ AccountMenager/Models/Identity/RegisterViewModel.cs
@@ -9,10 +9,13 @@ namespace AccountMenager.Models.Identity
         public string Email { get; set; }
         [Required(ErrorMessage = "Hasło jest wymagane!"),
         DataType(DataType.Password, ErrorMessage = "Hasło nie przeszło walidacji!"),
-        RegularExpression(@"^[A-Z][0-9A-Za-z.]{8,32}")]
+        RegularExpression(@"^[A-Z]{0,1}[!@#$%^&*()]{0,1}[0-9A-Za-z.]{6,32}", ErrorMessage ="Wymagana jedna duża litera a minimalna długość hasła to 8 znaków.")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Powtórne podanie hasła jest konieczne."),
+        DataType(DataType.Password, ErrorMessage = "Hasło nie przeszło walidacji!"),
         Compare(nameof(Password),ErrorMessage = "Hasła nie są identyczne.")] //TODO: Hasła nie są identyczne - podczas gdy fizycznie są
         public string ConfirmPassword { get; set; }
+        //[0-9A-Za-z!@#$%^&*().]{8,32}
+        //^([A-Z])([0-9A-Za-z!@#$%^&*().]){8,32}
     }
 }
